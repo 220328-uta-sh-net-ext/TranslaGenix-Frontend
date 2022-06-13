3068810607c8137e0753cde485ec266799c3c0d8
Briceson <royzone4@gmail.com>
2022-06-13 15:05:57 -0400

    dark mode progress progressing

--- translagenix/src/app/app.component.ts
+++ translagenix/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { ThemeService } from './dark-theme.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app-theme.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'translagenix';
