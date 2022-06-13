0e369a6565ec871780a28989c9839e4b2b7af09d
Briceson <royzone4@gmail.com>
2022-06-13 11:41:40 -0400

    beginning work on dark mode

--- translagenix/src/app/app.component.ts
+++ translagenix/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+//import { Service } from 'rxjs';
+import { ThemeService } from './dark-theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'translagenix';
+
+  isDarkTheme!: Observable<boolean>;
+
+  toggleDarkTheme(checked: boolean) {
+    this.themeService.setDarkTheme(checked);
+  }
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit() {
+    this.isDarkTheme = this.themeService.isDarkTheme;
+  }
 }
--- translagenix/src/app/navbar/navbar.component.ts
+++ translagenix/src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
 import { NavbarService } from '../navbar.service';
 
+
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
@@ -13,6 +14,8 @@ export class NavbarComponent implements OnInit {
     this.router.navigate(["home"])
   }
   constructor(public nav: NavbarService,private router:Router) { }
+
+
 
   ngOnInit(): void {
   }
